fa96e81a50a717e76d143b554fd4fc2e09d551ff
Tomso <114697260+BenjaminVeli@users.noreply.github.com>
2026-01-02 17:18:22 -0500

    bveli - initial commit

---
+++ app/routes/home.tsx
@@ -0,0 +1,44 @@
+import Navbar from "~/components/Navbar";
+import type { Route } from "./+types/home";
+import { resumes } from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+import { usePuterStore } from "lib/puter";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback for your dream" },
+  ];
+}
+
+export default function Home() {
+  const { auth } = usePuterStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+      if(auth.isAuthenticated) navigate('/auth?next=/'); 
+  }, [auth.isAuthenticated])
+
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    {/* {window.puter.ai.chat()} */}
+    
+    <section className= "main-section">
+      <div className="page-heading py-16">
+        <h1>Track Your Apllications & Resume Ratings</h1>
+        <h2>Review your submissions and check AI-powered feedback.</h2>
+      </div>
+    </section>
+
+    {resumes.length > 0 && (
+      <div className="resumes-section">
+        {resumes.map((resume) => (
+          <ResumeCard key={resume.id} resume={resume}/>
+        ))}
+      </div>
+    )}
+
+  </main>;
+}
